1e8f735846e58a9085080492742fa5389c08068c
Sahil bisht <sahilbisht19334@gmail.com>
2022-11-05 22:32:54 +0530

    added server

--- src/SocketContext.js
+++ src/SocketContext.js
@@ -1,12 +1,13 @@
 import React, { createContext, useEffect, useState, useRef } from "react";
 import { io } from 'socket.io-client'
 import Peer from 'simple-peer'
-
-
+import * as tf from '@tensorflow/tfjs'
+// import {loadGraphModel} from '@tensorflow/tfjs-converter';
+// const model_url='model.json'
+// const model = await loadGraphModel(model_url);
+// import modelWeight1 from "..server/model.json"
 
 const SocketContext = createContext()
-
-
 const socket = io('https://video-calling-app-react.herokuapp.com');
 
 
@@ -21,6 +22,48 @@ const ContextProvider=({children})=>{
     const userVideo=useRef()
     const connectionRef=useRef()
 
+
+
+
+    const detect = async (net) => {
+        // Check data is available
+       
+    
+          // 4. TODO - Make Detections
+          const img=tf.browser.fromPixels(myVideo)
+          const resized=tf.image.resizeBilinear(img,[640,480])
+          const casted=resized.cast('int32')
+          const expanded=casted.expandDims(0)
+          const obj=await net.executeAsync(expanded)
+          console.log(obj)
+    
+    
+    
+          // Draw mesh
+        //   const ctx = canvasRef.current.getContext("2d");
+    
+          // 5. TODO - Update drawing utility
+          // drawSomething(obj, ctx)  
+    
+          tf.dispose(img)
+          tf.dispose(resized)
+          tf.dispose(casted)
+          tf.dispose(expanded)
+          tf.dispose(obj)
+        }
+    
+        const runCoco=async ()=>{
+            const net= await tf.loadGraphModel('http://localhost:5000/model.json')
+            
+        
+        
+            setInterval(()=>{
+                detect(net);
+        
+            },16.7)
+        }
+//   useEffect(()=>{runCoco()},[]);
+    
 
     useEffect(()=>{
         navigator.mediaDevices.getUserMedia({video:true,audio:true})
